357b5b8900b7caa890374fcae710b8e7241bb1a1
Mohammad Yasin Karbasian <82156191+MohammadYasinKarbasian@users.noreply.github.com>
2024-10-02 14:37:44 +0330

    Upload Files

---
+++ Server/Server/Server/Config.cs
@@ -0,0 +1,15 @@
+namespace Server;
+
+public static class Config
+{
+ 
+    public const string Server = "localhost";
+    public const string DataBase = "etl";
+    public const string Id = "sa";
+    public const string Password = "1234";
+    public const string dataInventoryTableName = "AllDatasetsInventory";
+    
+    public const int PreviewCapacity = 20;  
+    public const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+
+}
